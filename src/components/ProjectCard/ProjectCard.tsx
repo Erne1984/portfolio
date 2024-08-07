8d863502b04ca505dd5108644c5cd297679ae627
Ernesto <ernestofamorim@gmail.com>
2024-08-07 10:56:53 -0300

    feat: project card flip animation

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,22 +1,27 @@
 import './ProjectCard.css';
 
-interface ProjectCardProps{
+interface ProjectCardProps {
     projectTitle: string,
     projectDescri: string,
-    projectPrintUrl: string
+    projectPrintUrl: string,
+    repoUrl: string,
+    previewUrl: string
 }
 
-export default function ProjectCard(props: ProjectCardProps){
-
-    return(
-        <div className="card-project">
-
-            <img src={props.projectPrintUrl}></img>
-
-            <h3>{props.projectTitle}</h3>
-
-            <p>{props.projectDescri}</p>
-            
+export default function ProjectCard(props: ProjectCardProps) {
+    return (
+        <div className="card-wrapper">
+            <div className="card-project">
+                <div className="card-front">
+                    <img src={props.projectPrintUrl} alt={props.projectTitle} />
+                    <h3>{props.projectTitle}</h3>
+                    <p>{props.projectDescri}</p>
+                </div>
+                <div className="card-back">
+                    <a href={props.repoUrl} target="_blank" rel="noopener noreferrer">Ver Repositório</a>
+                    <a href={props.previewUrl} target="_blank" rel="noopener noreferrer">Ver Preview</a>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
--- src/layouts/ProjectsSection/ProjectsSection.tsx
+++ src/layouts/ProjectsSection/ProjectsSection.tsx
@@ -1,19 +1,26 @@
+import './ProjectsSection.css';
+
 import ProjectCard from "../../components/ProjectCard/ProjectCard"
 
 export default function ProjectsSection() {
 
     return (
-        <section>
+        <section className="project-section-container">
 
             <div className='section-title'>
                 <h1>Projetos</h1>
             </div>
 
-            <ProjectCard 
-            projectDescri="Projeto mais pica da história" 
-            projectPrintUrl={'/assets/projectsPrints/globalExplorer.png'}
-            projectTitle="Global Explorer"/>
+            <div className='projects-cards'>
 
+                <ProjectCard
+                    projectDescri="Aplicação desenvolvida em React que lista todos os países do mundo com várias informações a respeito de cada um. É um desafio do site Frontend Mentor."
+                    projectPrintUrl={'/assets/projectsPrints/globalExplorer.png'}
+                    projectTitle="Global Explorer"
+                    previewUrl='https//:google.com'
+                    repoUrl='htteps' />
+
+            </div>
 
         </section>
     )
