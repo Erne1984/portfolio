5d9129eb6cafc3b21257bcb487f4f8a248f6b810
Erne1984 <ernestofamorim@gmail.com>
2024-08-08 20:11:30 -0300

    refactor: responsiveness in some components

--- src/components/ProjectsCarousel/ProjectsCarousel.tsx
+++ src/components/ProjectsCarousel/ProjectsCarousel.tsx
@@ -18,16 +18,7 @@ export default function ProjectsCarousel() {
         slidesToScroll: 3,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
+                breakpoint: 1034,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
@@ -35,7 +26,7 @@ export default function ProjectsCarousel() {
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 834,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
